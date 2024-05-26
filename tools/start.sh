d30bdb5eba6792fa1d01d93d0b8cd4c1b9d38c9b
Unedo Tampubolon <unedo.tampubolon@gmail.com>
2024-05-26 11:06:26 +0700

    fix: docker compose

--- tools/start.sh
+++ tools/start.sh
@@ -3,6 +3,8 @@
 echo "start wait-for-it"
 
 set -e
+echo "ensure connection"
+while ! nc -z "${PGHOST}" "${POSTGRES_PORT}"; do sleep 1; done 
 
 echo "start migrate"
 migrate -path ./migration -database "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$PGHOST:$POSTGRES_PORT/$POSTGRES_DB?sslmode=disable" --verbose up
